d561220ccbe96cb6086d2d46d49d3ad8e1eb942d
hengist wang <hengistwang@gmail.com>
2025-03-07 00:44:22 +0800

    Hello, world!

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
