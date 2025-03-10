d0e03bd53569f21bf0b919197916931f3bd532e7
hengist wang <hengistwang@gmail.com>
2025-03-10 19:42:12 +0800

    vga buffer

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
-    loop {}
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
 }
